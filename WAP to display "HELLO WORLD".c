9d6b1ccfa89b205fc65491ff1465f6822648c792
VijaycodeON <vijayofficial19@gmail.com>
2025-06-14 07:41:31 +0530

    Add files via upload

---
+++ WAP to display "HELLO WORLD".c
@@ -0,0 +1,13 @@
+/*---------------------------------------------
+DESCRIPTION : WAP to display "HELLO WORLD".
+----------------------------------------------*/
+
+//header file inclusion
+#include<stdio.h>
+
+//main function
+void main()
+{
+    //message "hello world" to be printed in the output terminal
+    printf("HELLO WORLD\n");
+}
